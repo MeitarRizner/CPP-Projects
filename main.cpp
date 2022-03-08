b2f97389c91bedc1cea3c7c8ca603d45ceb2e574
Meitar Rizner <101214168+MeitarRizner@users.noreply.github.com>
2022-03-08 22:08:38 +0200

    Delete main.cpp

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "Game.h"
-
-//The main program creates a game object and starts the game.
-int main() {
-    Game* game = new Game();
-    game->Play();
-    return 0;
-}
